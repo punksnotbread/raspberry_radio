0ccb4ac1559c9f4a13e4f310367e3a332e64da1a
Tomas Gilys <tomas.gilys@oxylabs.io>
2021-12-13 19:34:01 +0200

    Initial commit

---
+++ radio.py
@@ -0,0 +1,51 @@
+import logging
+from signal import pause
+
+from gpiozero import Button
+
+from library import OPTIONS, RADIOS
+from player import Player
+
+logging.basicConfig(format="%(asctime)s %(name)s  %(levelname)s  %(message)s")
+_logger = logging.getLogger("radio_button")
+_logger.level = logging.DEBUG
+_logger.debug("Initialised radio button listener.")
+
+
+class Radio(Player):
+    def __init__(self, player):
+        self.enabled = False
+        self.hold_time = 0
+        self.gpio_pin = 14
+        self.button = Button(self.gpio_pin, hold_time=self.hold_time)
+        self.wait_time = 2
+        self.player = player
+
+    def _get_option(self, num: int) -> str:
+        return OPTIONS[(num - 1) % len(OPTIONS)]
+
+    def _run_command(self, option: int) -> None:
+        radio = RADIOS.get(option, None)
+        if radio:
+            self.player.play_file(radio["name"])
+            self.player.play_url(radio["url"])
+        else:
+            self.player.play_file("off")
+            self.player.stop()
+
+    def _listen_button(self):
+        press_count = 0
+        while True:
+            pressed = self.button.wait_for_press(timeout=self.wait_time)
+            self.button.wait_for_release()
+            if pressed:
+                press_count += 1
+                _logger.debug(
+                    f"Registered a button press, current count is {press_count}"
+                )
+                option = self._get_option(press_count)
+                self._run_command(option)
+
+    def run(self):
+        self._listen_button()
+        pause()
